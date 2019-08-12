e0a7605129961079daa7cafb18ed91b571e6a46c
adv7 <cud4xx@gmail.com>
2019-08-12 13:48:20 +0200

    adding neighbours detection to field

--- Assets/PathFinder.cs
+++ Assets/PathFinder.cs
@@ -8,12 +8,36 @@ public class PathFinder : MonoBehaviour
     [SerializeField] Waypoint startWaypont, endWaypoint;
 
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Vector2Int[] directions =
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
 
     // Start is called before the first frame update
     void Start()
     {
         LoadBlocks();
         ColorStartAndEnd();
+        ExploreNeighbours();
+    }
+
+    private void ExploreNeighbours()
+    {
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int explorationCoordinates = direction + startWaypont.GetGridPos();
+            try
+            {
+                grid[explorationCoordinates].SetTopColor(Color.blue);
+            }
+            catch
+            {
+                // do nothing
+            }
+        }
     }
 
     private void ColorStartAndEnd()
