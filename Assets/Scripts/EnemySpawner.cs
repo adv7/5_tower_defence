35c15db593c7cf645dd7ff2d4d641b6d754db620
adv7 <cud4xx@gmail.com>
2019-08-16 14:00:09 +0200

    starting adding enemyspawning

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float secondsBetweenSpawns = 3f;
+    [SerializeField] EnemyMovement enemyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RepeateddlySpawnEnemy());
+    }
+
+    IEnumerator RepeateddlySpawnEnemy()
+    {
+        while(true)
+        {
+            print("enemy spawned");
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }        
+    }
+}
