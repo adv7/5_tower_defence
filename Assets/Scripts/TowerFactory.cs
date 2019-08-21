376297a5c7f79eadfac53593457ec49f087010fb
adv7 <cud4xx@gmail.com>
2019-08-21 14:53:52 +0200

    adding towers limit on scene

---
+++ Assets/Scripts/TowerFactory.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerFactory : MonoBehaviour
+{
+    [SerializeField] int towerLimit = 5;
+    [SerializeField] Tower towerPrefab;
+
+    int numTowers = 0;
+
+    public void AddTower(Waypoint baseWaypoint)
+    {
+        if(numTowers < towerLimit)
+        {
+            InstantiateNewTower(baseWaypoint);
+        }
+        else
+        {
+            MoveExistingTower();
+        }
+    }
+
+    private static void MoveExistingTower()
+    {
+        Debug.Log("to many towers on scene");
+        // todo actually move
+    }
+
+    private void InstantiateNewTower(Waypoint baseWaypoint)
+    {
+        numTowers++;
+        Instantiate(towerPrefab, baseWaypoint.transform.position, Quaternion.identity);
+        baseWaypoint.isPlaceable = false;
+    }
+}
